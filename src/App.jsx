6867e0e0b2fc0921634684f2494dedb1ef887833
Pugazhendhi <pugazhendhiexplorer@gmail.com>
2025-03-20 09:16:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./auth/signin.jsx";
 import QrScanner from "./pages/qrscanner.jsx";
 import Checkqr from "./pages/checkqr.jsx";
-import Viewlogs from "./pages/viewlogs.jsx";
 
 function App() {
   return (
